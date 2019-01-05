0e6a694f7802db9a2805c812a696b45502b5c0ed
purkialo <purkialo@gmail.com>
2019-01-05 14:59:19 +0800

    refresh

---
+++ Homework/Data Analysis Tool/TF_resnet_flower.py
@@ -0,0 +1,231 @@
+# encoding: utf-8
+from skimage import io,transform
+import glob
+import os
+import tensorflow as tf
+import numpy as np
+import time
+from tensorflow.python.training import moving_averages
+
+#数据集地址
+path='./flower_photos/'
+#模型保存地址
+model_path='./model.ckpt'
+
+#将所有的图片resize成224*224
+w=100
+h=100
+c=3
+
+#读取图片
+def read_img(path):
+    cate=[path+x for x in os.listdir(path) if os.path.isdir(path+x)]
+    imgs=[]
+    labels=[]
+    for idx,folder in enumerate(cate):
+        for im in glob.glob(folder+'/*.jpg'):
+            print('reading the images:%s'%(im))
+            img=io.imread(im)
+            img=transform.resize(img,(w,h))
+            imgs.append(img)
+            labels.append(idx)
+    return np.asarray(imgs,np.float32),np.asarray(labels,np.int32)
+data,label=read_img(path)
+
+#打乱顺序
+num_example=data.shape[0]
+arr=np.arange(num_example)
+np.random.shuffle(arr)
+data=data[arr]
+label=label[arr]
+
+#将所有数据分为训练集和验证集
+ratio=0.8
+s=np.int(num_example*ratio)
+x_train=data[:s]
+y_train=label[:s]
+x_val=data[s:]
+y_val=label[s:]
+
+#-----------------构建网络----------------------
+#占位符
+x=tf.placeholder(tf.float32,shape=[None,w,h,c],name='x')
+y_=tf.placeholder(tf.int32,shape=[None,],name='y_')
+
+fc_initializer = tf.contrib.layers.xavier_initializer
+conv2d_initializer = tf.contrib.layers.xavier_initializer_conv2d
+
+# create weight variable
+def create_var(name, shape, initializer, trainable=True):
+    return tf.get_variable(name, shape=shape, dtype=tf.float32,
+                           initializer=initializer, trainable=trainable)
+
+# conv2d layer
+def conv2d(x, num_outputs, kernel_size, stride=1, scope="conv2d"):
+    num_inputs = x.get_shape()[-1]
+    with tf.variable_scope(scope):
+        kernel = create_var("kernel", [kernel_size, kernel_size,
+                                       num_inputs, num_outputs],
+                            conv2d_initializer())
+        return tf.nn.conv2d(x, kernel, strides=[1, stride, stride, 1],
+                            padding="SAME")
+
+# fully connected layer
+def fc(x, num_outputs, scope="fc"):
+    num_inputs = x.get_shape()[-1]
+    with tf.variable_scope(scope):
+        weight = create_var("weight", [num_inputs, num_outputs],
+                            fc_initializer())
+        bias = create_var("bias", [num_outputs,],
+                          tf.zeros_initializer())
+        return tf.nn.xw_plus_b(x, weight, bias)
+
+# batch norm layer
+def batch_norm(x, decay=0.999, epsilon=1e-03, is_training=True,
+               scope="scope"):
+    x_shape = x.get_shape()
+    num_inputs = x_shape[-1]
+    reduce_dims = list(range(len(x_shape) - 1))
+    with tf.variable_scope(scope):
+        beta = create_var("beta", [num_inputs,],
+                               initializer=tf.zeros_initializer())
+        gamma = create_var("gamma", [num_inputs,],
+                                initializer=tf.ones_initializer())
+        # for inference
+        moving_mean = create_var("moving_mean", [num_inputs,],
+                                 initializer=tf.zeros_initializer(),
+                                 trainable=False)
+        moving_variance = create_var("moving_variance", [num_inputs],
+                                     initializer=tf.ones_initializer(),
+                                     trainable=False)
+    if is_training:
+        mean, variance = tf.nn.moments(x, axes=reduce_dims)
+        update_move_mean = moving_averages.assign_moving_average(moving_mean,
+                                                mean, decay=decay)
+        update_move_variance = moving_averages.assign_moving_average(moving_variance,
+                                                variance, decay=decay)
+        tf.add_to_collection(tf.GraphKeys.UPDATE_OPS, update_move_mean)
+        tf.add_to_collection(tf.GraphKeys.UPDATE_OPS, update_move_variance)
+    else:
+        mean, variance = moving_mean, moving_variance
+    return tf.nn.batch_normalization(x, mean, variance, beta, gamma, epsilon)
+
+# avg pool layer
+def avg_pool(x, pool_size, scope):
+    with tf.variable_scope(scope):
+        return tf.nn.avg_pool(x, [1, pool_size, pool_size, 1],
+                strides=[1, pool_size, pool_size, 1], padding="VALID")
+
+# max pool layer
+def max_pool(x, pool_size, stride, scope):
+    with tf.variable_scope(scope):
+        return tf.nn.max_pool(x, [1, pool_size, pool_size, 1],
+                              [1, stride, stride, 1], padding="SAME")
+
+class ResNet50(object):
+    def __init__(self, inputs, num_classes=5, is_training=True,
+                 scope="resnet50"):
+        self.inputs =inputs
+        self.is_training = is_training
+        self.num_classes = num_classes
+
+        with tf.variable_scope(scope):
+            # construct the model
+            net = conv2d(inputs, 64, 7, 2, scope="conv1") # -> [batch, 50, 50, 64]
+            net = tf.nn.relu(batch_norm(net, is_training=self.is_training, scope="bn1"))
+            net = max_pool(net, 3, 2, scope="maxpool1")  # -> [batch, 25, 25, 64]
+            net = self._block(net, 256, 3, init_stride=1, is_training=self.is_training,
+                              scope="block2")           # -> [batch, 25, 25, 256]
+            net = self._block(net, 512, 4, is_training=self.is_training, scope="block3")
+                                                        # -> [batch, 13, 13, 512]
+            net = self._block(net, 1024, 6, is_training=self.is_training, scope="block4")
+                                                        # -> [batch, 7, 7, 1024]
+            net = self._block(net, 2048, 3, is_training=self.is_training, scope="block5")
+                                                        # -> [batch, 4, 4, 2048]
+            net = avg_pool(net, 4, scope="avgpool5")    # -> [batch, 1, 1, 2048]
+            net = tf.squeeze(net, [1, 2], name="SpatialSqueeze") # -> [batch, 2048]
+            self.logits = fc(net, self.num_classes, "fc6")       # -> [batch, num_classes]
+            self.predictions = tf.nn.softmax(self.logits)
+
+
+    def _block(self, x, n_out, n, init_stride=2, is_training=True, scope="block"):
+        with tf.variable_scope(scope):
+            h_out = n_out // 4
+            out = self._bottleneck(x, h_out, n_out, stride=init_stride,
+                                   is_training=is_training, scope="bottlencek1")
+            for i in range(1, n):
+                out = self._bottleneck(out, h_out, n_out, is_training=is_training,
+                                       scope=("bottlencek%s" % (i + 1)))
+            return out
+
+    def _bottleneck(self, x, h_out, n_out, stride=None, is_training=True, scope="bottleneck"):
+        """ A residual bottleneck unit"""
+        n_in = x.get_shape()[-1]
+        if stride is None:
+            stride = 1 if n_in == n_out else 2
+
+        with tf.variable_scope(scope):
+            h = conv2d(x, h_out, 1, stride=stride, scope="conv_1")
+            h = batch_norm(h, is_training=is_training, scope="bn_1")
+            h = tf.nn.relu(h)
+            h = conv2d(h, h_out, 3, stride=1, scope="conv_2")
+            h = batch_norm(h, is_training=is_training, scope="bn_2")
+            h = tf.nn.relu(h)
+            h = conv2d(h, n_out, 1, stride=1, scope="conv_3")
+            h = batch_norm(h, is_training=is_training, scope="bn_3")
+
+            if n_in != n_out:
+                shortcut = conv2d(x, n_out, 1, stride=stride, scope="conv_4")
+                shortcut = batch_norm(shortcut, is_training=is_training, scope="bn_4")
+            else:
+                shortcut = x
+            return tf.nn.relu(shortcut + h)
+
+#---------------------------网络结束---------------------------
+logits = ResNet50(x).logits
+loss=tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=y_)
+train_op=tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
+correct_prediction = tf.equal(tf.cast(tf.argmax(logits,1),tf.int32), y_)    
+acc= tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+#定义一个函数，按批次取数据
+def minibatches(inputs=None, targets=None, batch_size=None, shuffle=False):
+    assert len(inputs) == len(targets)
+    if shuffle:
+        indices = np.arange(len(inputs))
+        np.random.shuffle(indices)
+    for start_idx in range(0, len(inputs) - batch_size + 1, batch_size):
+        if shuffle:
+            excerpt = indices[start_idx:start_idx + batch_size]
+        else:
+            excerpt = slice(start_idx, start_idx + batch_size)
+        yield inputs[excerpt], targets[excerpt]
+
+#训练和测试数据，可将n_epoch设置更大一些
+
+n_epoch=10
+batch_size=64
+saver=tf.train.Saver()
+sess=tf.Session()
+sess.run(tf.global_variables_initializer())
+for epoch in range(n_epoch):
+    start_time = time.time()
+
+    #training
+    train_loss, train_acc, n_batch = 0, 0, 0
+    for x_train_a, y_train_a in minibatches(x_train, y_train, batch_size, shuffle=True):
+        _,err,ac=sess.run([train_op,loss,acc], feed_dict={x: x_train_a, y_: y_train_a})
+        train_loss += err; train_acc += ac; n_batch += 1
+    print("   train loss: %f" % (np.sum(train_loss)/ n_batch))
+    print("   train acc: %f" % (np.sum(train_acc)/ n_batch))
+
+    #validation
+    if(epoch % 3 == 0):
+        val_loss, val_acc, n_batch = 0, 0, 0
+        for x_val_a, y_val_a in minibatches(x_val, y_val, batch_size, shuffle=False):
+            err, ac = sess.run([loss,acc], feed_dict={x: x_val_a, y_: y_val_a})
+            val_loss += err; val_acc += ac; n_batch += 1
+        print("   validation loss: %f" % (np.sum(val_loss)/ n_batch))
+        print("   validation acc: %f" % (np.sum(val_acc)/ n_batch))
+        saver.save(sess,model_path)
+sess.close()
